4023d9054138c8563a4a6ddec23977af443466cd
moonyl <moonysl@hanmail.net>
2018-10-20 04:11:18 +0900

    check leveldb

--- lotto-gen.pro
+++ lotto-gen.pro
@@ -3,7 +3,9 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 CONFIG += link_pkgconfig
-PKGCONFIG += curlpp jsoncpp
+PKGCONFIG += jsoncpp curlpp
 
+DEFINES += _GLIBCXX_USE_CXX11_ABI
 SOURCES += \
         main.cpp
+LIBS += -lleveldb
--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include <leveldb/db.h>
 
 using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::ostringstream;
 
 int main()
 {
@@ -16,24 +20,70 @@ int main()
     // Here I use a shortcut to get it in a string stream ...
 
     std::ostringstream os;
-    os << curlpp::options::Url(std::string("http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=3"));
 
+    //os << curlpp::options::Url(string("http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=3"));
+    os << curlpp::options::Url("http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=3");
     string asAskedInQuestion = os.str();
     std::cout << asAskedInQuestion << std::endl;
 
-    Json::Value root;
+    Json::Value lottoJson;
     std::istringstream strStream(asAskedInQuestion);
-    strStream >> root;
+    strStream >> lottoJson;
 
-    std::cout << root << std::endl;
+    std::cout << lottoJson << std::endl;
 
     std::cout << "check element" << std::endl;
-    std::cout << root["drwtNo1"] << std::endl;
-    std::cout << root["drwtNo1"].asUInt() << std::endl;
+    std::cout << lottoJson["drwtNo1"] << std::endl;
+    std::cout << lottoJson["drwtNo1"].asUInt() << std::endl;
 
     //auto no1 = root["drwtNo1"];
     //std::cout << typeid(no1).name() << std::endl;
 
+    leveldb::DB* db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+
+    leveldb::Status status = leveldb::DB::Open(options, "./testdb", &db);
+
+    if (false == status.ok())
+    {
+        cerr << "Unable to open/create test database './testdb'" << endl;
+        cerr << status.ToString() << endl;
+        return -1;
+    }
+
+    // Add 256 values to the database
+    leveldb::WriteOptions writeOptions;
+    //for (unsigned int i = 0; i < 256; ++i)
+    //{
+        ostringstream keyStream;
+        //keyStream << "Key" << i;
+        keyStream << 0;
+
+        ostringstream valueStream;
+        valueStream << lottoJson;
+
+        db->Put(writeOptions, keyStream.str(), valueStream.str());
+    //}
+
+    // Iterate over each item in the database and print them
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+
+    for (it->SeekToFirst(); it->Valid(); it->Next())
+    {
+        cout << it->key().ToString() << " : " << it->value().ToString() << endl;
+    }
+
+    if (false == it->status().ok())
+    {
+        cerr << "An error was found during the scan" << endl;
+        cerr << it->status().ToString() << endl;
+    }
+
+    delete it;
+
+    // Close the database
+    delete db;
 
     return 0;
 }
